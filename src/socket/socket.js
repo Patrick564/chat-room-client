b69b0d32562100d9b94360478e1512f791a6c77e
Patrick Vilchez <pvilchez794@gmail.com>
2022-02-11 23:15:35 -0500

    Cors methods added

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client'
 
 const server = process.env.REACT_APP_SOCKET_SERVER
-const socket = io(server)
+const socket = io(server, {
+  withCredentials: true,
+})
 
 export default socket
