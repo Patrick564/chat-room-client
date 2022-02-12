5ddecbb9c7814556c0e3de94b1ea217c4169cce9
Patrick Vilchez <pvilchez794@gmail.com>
2022-02-12 08:39:35 -0500

    Socketio credentials removed

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -4,7 +4,7 @@ const server = process.env.REACT_APP_SOCKET_SERVER
 
 // const server = ['*', 'https://chat-server-vercel.herokuapp.com/']
 const socket = io(server, {
-  withCredentials: true,
+  // withCredentials: true,
 })
 
 export default socket
