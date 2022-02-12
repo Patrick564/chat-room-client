46ec1828215f3989d6499782b4b3ea6bbf0dbe41
Patrick Vilchez <pvilchez794@gmail.com>
2022-02-12 00:06:21 -0500

    Cors env var changed

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client'
 
-// const server = process.env.REACT_APP_SOCKET_SERVER
+const server = process.env.REACT_APP_SOCKET_SERVER
 
-const server = ['https://chat-room-client-psi.vercel.app/', 'https://chat-server-vercel.herokuapp.com/']
+// const server = ['*', 'https://chat-server-vercel.herokuapp.com/']
 const socket = io(server, {
   withCredentials: true,
 })
