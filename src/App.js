118465f345214800ce467c6aa0b6bd7e12624419
Patrick Vilchez <pvilchez794@gmail.com>
2022-02-11 23:25:17 -0500

    Test env vars

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './components/Login/login.js'
 import './App.css'
 
 function App() {
+  console.log(process.env.REACT_APP_SOCKET_SERVER);
   return (
     <Routes>
       <Route exact path={'/'} element={<Login />} />
